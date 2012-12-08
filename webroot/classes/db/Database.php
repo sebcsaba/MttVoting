7e47fbd75e9c84f90ebe6889c4c15924d33a275e
sebcsaba <sebcsaba@freemail.hu>
2012-12-08 21:02:30 +0100

    database classes enhanced

--- webroot/classes/db/Database.php
+++ webroot/classes/db/Database.php
@@ -1,31 +1,115 @@
 <?php
 
-class Database {
-
-	public function query(QueryBuilder $query) {
-		// TODO
-		// @return iterable
-		return array();
+/**
+ * Egy SQL adatbázis-kapcsolatot valósít meg, és magasabb szintű szolgáltatásokat tesz köré.
+ * Ha paraméterezett SQL-t használunk, akkor a paraméterek helyére ? kerüljön.
+ * 
+ * @author sebcsaba
+ */
+class Database extends LowDatabase {
+	
+	/**
+	 * SQL lekérdezés első sorát adja vissza (mint asszociatív tömöt)
+	 *
+	 * @param SQL $query A végrehajtandó SQL parancs
+	 * @param boolean $nullOnEmpty Ha igaz, akkor üres eredménynél null-t ad vissza, különben kivételt dob. 
+	 * @return array Az eredmény első sora, vagy null.
+	 * @throws DbException Ha nem sikerült a lekérdezést végrehajtani.
+	 * @throws DbException Ha üres eredményt kaptunk, és $nullOnEmpty hamis.
+	 */
+	public function queryFirstRow(SQL $query, $nullOnEmpty=false) {
+		$result = $this->queryNative($query);
+		$row = $this->engine->fetchFirstRowOnly($result);
+		if (!is_null($row)) {
+			return $row;
+		} else if ($nullOnEmpty) {
+			return null;
+		} else {
+			throw new DbException('the query return empty resultset', $query);
+		}
 	}
 	
-	public function queryCell(QueryBuilder $query) {
-		// TODO
-		// @return scalar
-		return null;
+	/**
+	 * SQL lekérdezés első sorának egy celláját adja vissza.
+	 *
+	 * @param SQL $query A végrehajtandó SQL parancs
+	 * @param boolean $nullOnEmpty Ha igaz, akkor üres eredménynél null-t ad vissza, különben kivételt dob.
+	 * @param string $fieldName A visszaadandó mező neve. Ha null, akkor sorrend szerint az elsőt adja.
+	 * @return mixed Az eredmény első sorának adott cellája, vagy null.
+	 * @throws DbException Ha nem sikerült a lekérdezést végrehajtani.
+	 * @throws NoSuchDataException Ha üres eredményt kaptunk, és $nullOnEmpty hamis.
+	 */
+	public function queryCell(SQL $query, $fieldName=null, $nullOnEmpty=false) {
+		$row = $this->queryFirstRow($query, $nullOnEmpty);
+		if ($row===null) {
+			return null;
+		}
+		return $this->getRowField($row,$fieldName);
 	}
 	
-	public function queryRow(QueryBuilder $query) {
-		// TODO
-		// @return arrya
-		return array();
+	/**
+	 * SQL lekérdezés adott oszlopát adja vissza egy tömbként
+	 *
+	 * @param SQL $query A végrehajtandó SQL parancs
+	 * @param string $fieldName A visszaadandó mező neve. Ha null, akkor sorrend szerint az elsőt adja.
+	 * @return array Az eredmény celláit tartalmazó tömb
+	 * @throws DbException Ha nem sikerült a lekérdezést végrehajtani.
+	 */
+	public function queryColumn(SQL $query, $fieldName = null) {
+		$result = array();
+		foreach ($this->query($query) as $row) {
+			$result []= $this->getRowField($row,$fieldName);
+		}
+		return $result;
 	}
 	
-	public function update(UpdateBuilder $query) {
-		// TODO
+	/**
+	 * SQL lekérdezés eredményét asszociatív tömbként adja vissza 
+	 *
+	 * @param SQL $query A végrehajtandó SQL parancs
+	 * @param string $keyFieldName A kulcsokat tartalmazó mező neve. Ha null, akkor sorrend szerint az elsőt adja.
+	 * @param string $valueFieldName Az értékeket tartalmazó mező neve. Ha null, akkor sorrend szerint a másodikat adja.
+	 * @return array Az eredmény asszociatív tömbként
+	 * @throws DbException Ha nem sikerült a lekérdezést végrehajtani.
+	 */
+	public function queryMapping(SQL $query, $keyFieldName = null, $valueFieldName = null) {
+		$result = array();
+		foreach ($this->query($query) as $row) {
+			$key = $this->getRowField($row,$keyFieldName);
+			$value = $this->getRowField($row,$valueFieldName);
+			$result[$key] = $value;
+		}
+		return $result;
 	}
 	
-	public function insert($table, array $array) {
-		// TODO
+	/**
+	 * SQL lekérdezés eredményét asszociatív tömbök tömbjeként adja vissza 
+	 *
+	 * @param SQL $query A végrehajtandó SQL parancs
+	 * @return array Az eredmény tömb, amely minden eleme asszociatív tömb
+	 * @throws DbException Ha nem sikerült a lekérdezést végrehajtani.
+	 */
+	public function queryAssocTable(SQL $query) {
+		$result = array();
+		foreach ($this->query($query) as $row) {
+			$result []= $row;
+		}
+		return $result;
+	}
+	
+	/**
+	 * A kapott rekord megfelelő indexű elemét adja meg
+	 *
+	 * @param array $row Az adatbázis-rekord
+	 * @param string $fieldName A mező neve, vagy null
+	 * @return mixed A rekord adott mezőjű eleme, vagy a sorrend szerinti első, ha $fieldName null.
+	 */
+	private function getRowField(array &$row, $fieldName = null) {
+		if (is_null($fieldName)) {
+			return array_shift($row);
+		} else {
+			return $row[$fieldName];
+		}
 	}
 	
 }
---
+++ webroot/classes/db/LowDatabase.php
@@ -0,0 +1,120 @@
+<?php
+
+/**
+ * Egy SQL adatbázis-kapcsolatot valósít meg, engine-függetlenül.
+ * Ha paraméterezett SQL-t használunk, akkor a paraméterek helyére ? kerüljön.
+ * 
+ * @author sebcsaba
+ */
+class LowDatabase {
+	
+	/**
+	 * A kapcsolathoz használt adatbázis-motor
+	 *
+	 * @var DbEngine
+	 */
+	private $engine;
+	
+	/**
+	 * Adatbázis-kapcsolat létrehozása
+	 *
+	 * @param DbEngine $engine A kapcsolathoz használt adatbázis-motor
+	 */
+	public function __construct(DbEngine $engine) {
+		$this->engine = $engine;
+	}
+	
+	/**
+	 * Lezárja az adatbázis-kapcsolatot
+	 */
+	public function close() {
+		$this->engine->close();
+	}
+	
+	/**
+	 * Tranzakciót indít. Ha már korábban tranzakcióban voltunk, akkor növel egy számlálót,
+	 * hogy számon tartsa, hogy hány "beágyazott" tranzakciót kezdtünk meg.
+	 * 
+	 * @throws DbException Ha nem sikerült a parancsot végrehajtani.
+	 */
+	public function startTransaction() {
+		$this->engine->startTransaction();
+	}
+	
+	/**
+	 * Lezárja a tranzakciót. Ez csak akkor eredményez tényleges adatbázisbeli COMMIT-ot,
+	 * ha az utolsó (legkülső szintű) tranzakció kerül lezárásra. Ellenkező esetben csak a tranzakciós számlálót csökkentjük.
+	 * 
+	 * @throws DbException Ha nem sikerült a parancsot végrehajtani.
+	 */
+	public function commit() {
+		$this->engine->commit();
+	}
+	
+	/**
+	 * Visszavonja a tranzakciót. Ez az összes megkezdett tranzakciót hatástalanítja.
+	 * 
+	 * @throws DbException Ha nem sikerült a parancsot végrehajtani.
+	 */
+	public function rollback() {
+		$this->engine->rollback();
+	}
+	
+	/**
+	 * Lezárja a tranzakciót, ha a számláló nem nulla.
+	 * Siker esetén új tranzakciót nyit, és a tranzakciós számlálót nem változtatja.
+	 * 
+	 * @throws DbException Ha nem sikerült a parancsot végrehajtani.
+	 */
+	public function forceCommitAndStartTransactionAgain() {
+		$this->engine->forceCommitAndStartTransactionAgain();
+	}
+
+	/**
+	 * A paraméterben megadott stringet előkészíti SQL-be illesztéshez
+	 *
+	 * @param string $string A bemeneti string
+	 * @return string Az eredmény, amelyben escape-elve vannak a kritikus karakterek (aposztróf, stb)
+	 */
+	public function escape($string) {
+		return $this->engine->escape($string);
+	}
+	
+	/**
+	 * SQL parancs futtatása a megadott paraméterekkel.
+	 *
+	 * @param SQL $query A végrehajtandó SQL parancs (? jelöléssel a paraméterek helye)
+	 * @return integer Az érintett sorok száma.
+	 * @throws DbException Ha nem sikerült a parancsot végrehajtani.
+	 */
+	public function exec(SQL $query) {
+		$preparedQuery = $this->engine->getDialect()->prepareQuery($query);
+		return $this->engine->execNative($preparedQuery);
+	}
+
+	/**
+	 * SQL lekérdezés futtatása a megadott paraméterekkel.
+	 *
+	 * @param SQL $query A lekérdezés adatai
+	 * @return resource Az eredményhalmaz. Engine-től függ hogy milyen válaszobjektumot kapunk
+	 * @throws DbException Ha nem sikerült a lekérdezést végrehajtani.
+	 */
+	private function queryNative(SQL $query) {
+		$preparedQuery = $this->engine->getDialect()->prepareQuery($query);
+		$result = $this->engine->queryNative($preparedQuery);
+		return $this->engine->testResult($result,$preparedQuery);
+	}
+	
+	/**
+	 * A paraméterben kapott SQL lekérdezés futtatása
+	 *
+	 * @param SQL $query A lekérdezés adatai
+	 * @param boolean $autoClose Ha végigért az iterátoron, lezárhatja-e a DbResultSet-et.
+	 * @return DbResultSet Az eredményhalmazt csomagoló objektum, amellyel a foreach szerkezetet használhatjuk.
+	 * @throws DbException Ha nem sikerült a lekérdezést végrehajtani.
+	 */
+	public function query(SQL $query, $autoClose=true) {
+		return $this->engine->getResultSet($this->queryNative($query), $autoClose);
+	}
+
+}
