962ca4a9dd033252e1e0dd58643795ec05bd8b0e
sebcsaba <sebcsaba@freemail.hu>
2012-12-28 14:03:27 +0100

    document VotingResultService.getResult

--- webroot/classes/services/VotingResultService.php
+++ webroot/classes/services/VotingResultService.php
@@ -4,9 +4,16 @@ interface VotingResultService {
 	
 	/**
 	 * Get results of the given voting.
+	 * 
+	 * The result contains one item for each possible answer of the given voting.
+	 * Each line contains the following keys:
+	 *     id: id of the answer
+	 *     title: title of the answer
+	 *     cnt: number of votes for the answer
+	 * Thre result is ordered by the 'cnt' field, descendant.
 	 *
 	 * @param Voting $voting
-	 * @return ??TODO??
+	 * @return array
 	 */
 	public function getResult(Voting $voting);
 	
