aa07f3ece0656b75b8d14e1d6158b09baf1e4d35
sebcsaba <sebcsaba@freemail.hu>
2012-12-29 13:52:09 +0100

    update config files

---
+++ webroot/config/config.prod.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+
+	'di' => array(
+
+		'impl' => array(
+			'UserService' => 'TohuUserService',
+			'AuthenticationService' => 'TohuAuthenticationService',
+		),
+
+	),
+	
+);
