7cfdaefb0cdda055d8514aeca3697e1b295a35bd
sebcsaba <sebcsaba@freemail.hu>
2012-12-30 18:53:22 +0100

    voting status is printed to confirm_close screen

--- webroot/classes/actions/ConfirmCloseAction.php
+++ webroot/classes/actions/ConfirmCloseAction.php
@@ -6,9 +6,15 @@ class ConfirmCloseAction implements Action {
 	 * @var VotingListingService
 	 */
 	private $votingListingService;
+	
+	/**
+	 * @var VotingResultService
+	 */
+	private $votingResultService;
 
-	public function __construct(VotingListingService $votingListingService) {
+	public function __construct(VotingListingService $votingListingService, VotingResultService $votingResultService) {
 		$this->votingListingService = $votingListingService;
+		$this->votingResultService = $votingResultService;
 	}
 	
 	/**
@@ -26,7 +32,24 @@ class ConfirmCloseAction implements Action {
 			$request->setData('message', 'A megadott szavazás már lezárásra került!');
 			return new PageForward('error');
 		} else {
+			$request->setData('status', $this->getStatusText($voting));
 			return new PageForward('confirm_close');
+		}
+	}
+	
+	private function getStatusText(Voting $voting) {
+		$status = $this->votingResultService->getStatus($voting);
+		$totalCount = count($voting->getParticipants());
+		$notVotedCount = $status['not-voted-count'];
+		if ($notVotedCount==0) {
+			return sprintf('Minden szavazásra jogosult leadta szavazatát (%d személy).',$totalCount);
+		} else {
+			$text = sprintf('A szavazásra jogosult %d személy közül még nem szavazott %d személy', $totalCount, $notVotedCount);
+			if ($voting->getPrivate()) {
+				return $text . '.';
+			} else {
+				return $text . sprintf(': %s.', join(', ', $status['not-voted']));
+			}
 		}
 	}
 	
--- webroot/pages/confirm_close.tpl.php
+++ webroot/pages/confirm_close.tpl.php
@@ -2,10 +2,12 @@
 <div class="content-list-introtext">
 	<p class="important"><?h($request->getData('voting')->getTitle())?></p>
 	<p><?h($request->getData('voting')->getDescription())?></p>
+	<p class="important">A szavazás állapota:</p>
+	<p><?h($request->getData('status'))?></p>
 	<form id="edit_close" action="index.php" method="post" class="voteform">
 		<input type="hidden" name="do" value="Close"/>
 		<input type="hidden" name="id" value="<?h($request->getData('voting')->getId())?>"/>
-		<p>Biztosan le akarod zárni ezt a szavazást?</p>
+		<p class="important">Biztosan le akarod zárni ezt a szavazást?</p>
 		<input type="button" onclick="submitForm($('form#edit_close'));" value="Igen, biztosan lezárom"/>
 	</form>
 	<p><input type="button" onclick="openPage('EditVoting',{id:<?h($request->getData('voting')->getId())?>})" value="Nem, inkább mégsem zárom le"/></p>
