32cb992890c1aabe163a673f4bff7f8f94a11636
sebcsaba <sebcsaba@freemail.hu>
2013-01-02 02:01:08 +0100

    extend help

--- webroot/pages/help.tpl.php
+++ webroot/pages/help.tpl.php
@@ -1,15 +1,96 @@
-<div class="thcm-fp-title">
-	Szavazás használata
+<div class="thcm-fp-title">Szavazás használata</div>
+<div class="content-list-introtext">
+	<p>Ez itt az MTT vezetősége számára készített szavazóprogram.</p>
+	<p>A <b>bal oldalon</b> látod a számodra elérhető aktív szavazásokat.</p>
+	<p>Ha csak szavazni jöttél, akkor ezt kell használnod. A listában azokat a szavazásokat látod, amelyek:
+		<ul>
+			<li>még nyitottak, és nem szavaztál rájuk: menj és szavazz!</li>
+			<li>még nyitottak, és már szavaztál rájuk: meg kell várnod az eredménnyel a lezárását.</li>
+			<li>a legutóbbi lezártak, ekkor az eredményt látod.</li> 
+		</ul>
+	</p>
+	<p>Ha bármilyen technikai problémád van vele, <b><a href="https://www.facebook.com/sebcsaba">sebcsabát</a></b> keresd vele.</p>
 </div>
+
+<div class="thcm-fp-title">Szavazás adminisztrálása</div>
 <div class="content-list-introtext">
 	<p>
-		Ez itt az MTT vezetősége számára készített szavazóprogram.
+		A jobb oldali menüben tudod az általad kiírt szavazásokat adminisztrálni.
 	</p>
-	<p>
-		A jobb oldali menüben tudsz szavazásokat létrehozni, a korábban létrehozottakat
-		adminisztrálni.
+	<p>Új szavazás kiírásakor meg kell adni a következő adatokat:
+		<ul>
+			<li>Cím: ez fog a listában a résztvevőknek megjelenni</li>
+			<li>Leírás: opcionális, ide írhatod a részleteket vele kapcsolatban</li>
+			<li>Lehetséges választok: legalább kettőt meg kell adni.</li>
+			<li>Publikus vagy privát-e a szavazás. Publikus szavazás eredménye tartalmazni fogja, hogy ki hogyan szavazott, míg privát szavazásnál az eredmény nem fogja tartalmazni, hogy ki hogyan szavazott.</li>
+			<li>Résztvevők: a tolkien.hu felhasználói, akik szavazhatnak. Legalább három karakter begépelése után a felhasználói listából kiválasztható a keresett személy. A név mellett megjelenő x-re kattintva törölhető a résztvevő. Habár itt egy résztvevő többször is felvehető, ténylegesen csak egyszer kerül rögzítésre.</li>
+		</ul>
 	</p>
-	<p>
-		A bal oldalon látod a számodra elérhető aktív szavazásokat.
+	<p>Még nyitott szavazással a következőket lehet tenni:
+		<ul>
+			<li>Módosítani a címet és a leírást</li>
+			<li>További válaszlehetőséget hozzáadni</li>
+			<li>További résztvevőket felvenni a szavazáshoz</li>
+			<li>Lezárni. Lezárt szavazásnak csak az eredményét lehet megnézni.</li>
+			<li>Törölni.</li>
+		</ul>
 	</p>
+</div>
+
+<div class="thcm-fp-title">Alapszabály</div>
+<div class="content-list-introtext alapszabaly">
+	<p class="important">Kivonat a Magyar Tolkien Társaság alapszabályából
+		(2012. november 17-én elfogadott változat szerint):</p>
+	<div>
+		<span class="section">21.§. A TANÁCS</span>
+		<ol class="para" start="11">
+			<li>
+				A Tanács ülései alapértelmezetten személyes megjelenést igényelnek. Rendkívüli
+				sürgős esetben a Tanácsvezető jogosult ún „on-line” ülés összehívására,
+				amennyiben a következő technikai feltételek korlátozás nélkül biztosíthatóak
+				minden résztvevő számára:
+				<ol class="pont">
+					<li>Döntések előtti, valós idejű vita.</li>
+					<li>A vita rendjének, a hozzászólások sorrendjének szabályozhatósága a levezető elnök által.</li>
+					<li>Nyílt és titkos szavazások megvalósíthatósága.</li>
+					<li>A teljes ülés jegyzőkönyvezése.</li>
+					<li>Megbízható, stabil, manipulációt kizáró működési környezet.</li>
+				</ol>
+			</li>
+			<li>
+				Az „on-line” ülés összehívását a hagyományos üléshez hasonló módon, előzetes
+				meghívóval, az ülés előtt legalább 3 nappal lehet kezdeményezni. Az on-line
+				ülés csak akkor határozatképes, ha azon az Elnökség és a Felügyelő Bizottság
+				legalább egy-egy tagja részt vesz, és az ülés kezdetén a Tanács összes tagjának
+				legalább 2/3-a jóváhagyja az ülés ilyen formában történő lefolytatását.
+			</li>
+		</ol>
+		<span class="section">22.§. A TANÁCS</span>
+		<ol class="para" start="10">
+			<li>
+				Az Elnökség ülései alapértelmezetten személyes megjelenést igényelnek.
+				Rendkívüli sürgős esetben az összehívásra jogosultak ún. „on-line” ülés is
+				összehívhatnak, amennyiben a következő technikai feltételek korlátozás nélkül
+				biztosíthatóak minden résztvevő számára:
+				<ol class="pont">
+					<li>Döntések előtti, valós idejű vita.</li>
+					<li>A vita rendjének, a hozzászólások sorrendjének szabályozhatósága a levezető elnök által.</li>
+					<li>Nyílt és titkos szavazások megvalósíthatósága.</li>
+					<li>A teljes ülés jegyzőkönyvezése.</li>
+					<li>Megbízható, stabil, manipulációt kizáró működési környezet.</li>
+				</ol>
+			</li>
+			<li>
+				Az „on-line” ülés összehívását a hagyományos üléshez hasonló módon, előzetes
+				meghívóval, az ülés előtt legalább 2 nappal lehet kezdeményezni. Az on-line
+				ülés csak akkor határozatképes, ha azon az Elnökség mindhárom tagja, a
+				Főtitkár, a Gazdasági Vezető, a Felügyelő Bizottság legalább egy tagja
+				illetve a Tanács elé szánt előterjesztés tárgyalása esetén a Tanácsvezető
+				is részt vesz, és az ülés kezdetén az Elnökség mindhárom tagja jóváhagyja
+				az ülés ilyen formában történő lefolytatását.
+			</li>
+		</ol>
+	</div>
+	<ul>
+	</ul>
 </div>
